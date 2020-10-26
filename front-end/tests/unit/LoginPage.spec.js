68cb13a7b4eed143fecbb99dd62a86d2d87de397
inwoo <inwoox@naver.com>
2020-10-26 19:18:07 +0900

    프론트 , 백엔드 연결 (크로스 오리진을 처리하기 위해 프록시를 이용해 API 요청을 백엔드로 전달)

--- front-end/tests/unit/LoginPage.spec.js
+++ front-end/tests/unit/LoginPage.spec.js
@@ -1,9 +1,9 @@
 import { shallowMount } from '@vue/test-utils'
-import LoginPage from '@/components/LoginPage.vue'
+import LoginPage from '@/views/LoginPage.vue'
 
 describe('LoginPage.vue', () => {
   it('text maching test', () => {
-    const wrapper = shallowMount(HelloWorld);
+    const wrapper = shallowMount(LoginPage);
     expect(wrapper.vm.$el.querySelector('h1').textContent).toEqual('안녕하세요');
   })
 })
